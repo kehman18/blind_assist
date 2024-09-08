db6a68a96cb2d24e778654428e09df9a879a6f91
Daniel <kehindeadekola96@gmail.com>
2024-09-08 20:50:15 +0000

    streamlined it down

--- server.py
+++
@@ -1,7 +0,0 @@
-'''for permission'''
-import requests
-
-URL = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
-response = requests.get(URL, timeout=10)
-with open("imagenet_classes.txt", "w", encoding='utf-8') as f:
-    f.write(response.text)
