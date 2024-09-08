9a2af677a7bcb97b633f9e28e7789c935834cd8b
Daniel <kehindeadekola96@gmail.com>
2024-09-08 20:39:23 +0000

    updated the web app

--- README.md
+++
@@ -1,4 +0,0 @@
-# blind_assist
-
-
-This is a very light tho slow one i talked about and less complicated i just made it.
--- main.py
+++ main.py
@@ -1,38 +1,32 @@
 from flask import Flask, request, jsonify, send_from_directory
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 import io
-import os
 
 app = Flask(__name__)
 
-# Load the model and tokenizer
-model_id = "vikhyatk/moondream2"
-revision = "2024-08-26"
-model = AutoModelForCausalLM.from_pretrained(
-    model_id, trust_remote_code=True, revision=revision
-)
-tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+# Load the BLIP model and processor for image captioning
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    print(request)
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
-    
+
     file = request.files['file']
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
     try:
-        # Open the image
-        image = Image.open(io.BytesIO(file.read()))
-        print(image)
-        # Encode and process the image
-        enc_image = model.encode_image(image)
-        print("am here")
-        description = model.answer_question(enc_image, "Describe this image.", tokenizer)
-        print("am here")
+        # Open and preprocess the image
+        image = Image.open(io.BytesIO(file.read())).convert("RGB")
+        inputs = processor(images=image, return_tensors="pt")
+
+        # Generate description
+        outputs = model.generate(**inputs)
+        description = processor.decode(outputs[0], skip_special_tokens=True)
+
         return jsonify({'description': description}), 200
 
     except Exception as e:
